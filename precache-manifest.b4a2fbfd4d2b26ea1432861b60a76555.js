208e58f9980ed6d1d895344a7cceac41f4f051a5
Aline Vianna <alinealvesvianna@gmail.com>
2019-09-23 02:18:26 -0300

    Add link to site

---
+++ precache-manifest.b4a2fbfd4d2b26ea1432861b60a76555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074aa8198461a789e7c468e9f3f1d4ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f3c018ee0f0fff58cc1",
+    "url": "/static/css/main.1452f410.chunk.css"
+  },
+  {
+    "revision": "e4a95e458a6d2fce4ab3",
+    "url": "/static/js/2.1cebd763.chunk.js"
+  },
+  {
+    "revision": "6f3c018ee0f0fff58cc1",
+    "url": "/static/js/main.dc6fb5db.chunk.js"
+  },
+  {
+    "revision": "584e35b7b9ab21a57410",
+    "url": "/static/js/runtime-main.383248ff.js"
+  },
+  {
+    "revision": "8b6011e8bc9f21e0e6410691fe276057",
+    "url": "/static/media/Star_Wars_Logo.8b6011e8.png"
+  }
+]);
